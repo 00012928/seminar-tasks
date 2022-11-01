37f7dc560dc5052729226e4dfe56fef0f43e692c
00012928 <00012928@gmail.com>
2022-11-01 10:07:09 +0500

    seminar8 task 1

---
+++ seminar8task1.py
@@ -0,0 +1,6 @@
+student_1 = ('00012928', 18, 'Andijan')
+student_2 = ('00012222', 21, 'Tashkent')
+
+print(student_1[0] < student_2[0])
+print(student_1[1] < student_2[1])
+print(student_1[2] < student_2[2])
